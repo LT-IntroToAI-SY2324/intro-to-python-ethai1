a65b53d6283a8d474f25bc59524a71ea30a16ea3
Eric <ethai1@cps.edu>
2023-09-07 11:02:49 -0500

    rock, paper, scissors finished

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,15 +3,8 @@
 import random
 
 def player_won() -> bool:
-    """
-    PAPER : ROCK
 
-    SCISSORS : PAPER
-
-    ROCK : SCISSORS
-    """
-
-    if player_choice == "rocK" and computer_choice == "scissors":
+    if player_choice == "rock" and computer_choice == "scissors":
         return True
 
     if player_choice == "scissors" and computer_choice == "paper": 
@@ -20,21 +13,38 @@ def player_won() -> bool:
     if player_choice == "paper" and computer_choice == "rock":
         return True
     
+    if player_choice == computer_choice:
+        print("YOU DRAW!")
+        return False
+    
+    print("YOU LOSE!")
     return False
 
 def get_choices() -> tuple:
 
     options = ["rock", "paper", "scissors"]
 
-    player_choice = input("ROCK, PAPER, SCISSORS -> SHOOT!")
+    player_choice = input("ROCK, PAPER, SCISSORS - SHOOT! \n")
     computer_choice = random.choice(options)
 
     return player_choice, computer_choice
 
-player_choice, computer_choice = get_choices()
+playing = True
 
-if player_won():
-    print("YOU WIN!")
-else:
-    print("YOU LOSE!")
+while playing:
+    player_choice, computer_choice = get_choices()
+
+    if player_won():
+        print("YOU WIN!")
+
+    rematch = input("Want to play again? (y/n) \n")
+
+    if rematch == "y":
+        pass
+    elif rematch == "n":
+        playing = False
+    else:
+        print("Invalid response")
+        playing = False
+
 
